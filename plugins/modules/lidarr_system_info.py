29c698feddfa8d56485264df89eb35b0f831cde6
Fuochi <ffuochi@hotmail.com>
2023-03-10 11:29:25 +0400

    feat: first config

---
+++ plugins/modules/lidarr_system_info.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2020, Fuochi <devopsarr@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+
+DOCUMENTATION = r'''
+---
+module: lidarr_system_info
+
+short_description: Lidarr system info module
+
+version_added: "0.0.1"
+
+description: Provide Lidarr system info
+
+extends_documentation_fragment:
+    - devopsarr.lidarr.lidarr_credentials
+
+author:
+    - Fuochi (@Fuochi)
+'''
+
+EXAMPLES = r'''
+# It will fetch the system info
+- name: Test with a message
+  devopsarr.lidarr.lidarr_system_info:
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+app_name:
+    description: It should be Lidarr.
+    type: str
+    returned: always
+    sample: 'Lidarr'
+version:
+    description: Binary version.
+    type: str
+    returned: always
+    sample: '3.0.8.1507'
+build_time:
+    description: Build time.
+    type: str
+    returned: always
+    sample: '2022-04-23T21:40:53Z'
+is_debug:
+    description: Debug flag.
+    type: bool
+    returned: always
+    sample: 'false'
+is_production:
+    description: Production flag.
+    type: bool
+    returned: always
+    sample: 'false'
+is_admin:
+    description: Admin flag.
+    type: bool
+    returned: always
+    sample: 'false'
+is_user_interactive:
+    description: User interactive flag.
+    type: bool
+    returned: always
+    sample: 'false'
+startup_path:
+    description: Startup path.
+    type: str
+    returned: always
+    sample: '/app/lidarr/bin'
+app_data:
+    description: Configuration path.
+    type: str
+    returned: always
+    sample: '/config'
+os_name:
+    description: Host OS.
+    type: str
+    returned: always
+    sample: 'ubuntu'
+os_version:
+    description: Host OS version.
+    type: str
+    returned: always
+    sample: '20.04'
+is_mono_runtime:
+    description: Mono runtime flag.
+    type: bool
+    returned: always
+    sample: 'true'
+is_mono:
+    description: Mono flag.
+    type: bool
+    returned: always
+    sample: 'true'
+is_linux:
+    description: Linux flag.
+    type: bool
+    returned: always
+    sample: 'false'
+is_osx:
+    description: OSX flag.
+    type: bool
+    returned: always
+    sample: 'false'
+is_windows:
+    description: Windows flag.
+    type: str
+    returned: always
+    sample: 'false'
+mode:
+    description: Mode.
+    type: str
+    returned: always
+    sample: 'console'
+branch:
+    description: Lidarr branch.
+    type: str
+    returned: always
+    sample: 'master'
+authentication:
+    description: Authentication type.
+    type: str
+    returned: always
+    sample: 'none'
+sqlite_version:
+    description: SQLite version.
+    type: str
+    returned: always
+    sample: '3.31.1'
+url_base:
+    description: URI prefix for installation.
+    type: str
+    returned: always
+    sample: '/lidarr'
+runtime_version:
+    description: Runtime version.
+    type: str
+    returned: always
+    sample: '6.12.0.122'
+runtime_name:
+    description: Runtime name.
+    type: str
+    returned: always
+    sample: 'mono'
+start_time:
+    description: Start time.
+    type: str
+    returned: always
+    sample: '2022-06-09T08:21:23.970798Z'
+package_version:
+    description: Package version.
+    type: str
+    returned: always
+    sample: '3.0.8.1507-ls148'
+package_author:
+    description: Package author.
+    type: str
+    returned: always
+    sample: '[linuxserver.io](https://linuxserver.io)'
+package_update_mechanism:
+    description: Package update mechanism.
+    type: str
+    returned: always
+    sample: 'docker'
+'''
+
+from ansible_collections.devopsarr.lidarr.plugins.module_utils.lidarr_module import LidarrModule
+from ansible.module_utils.common.text.converters import to_native
+
+
+try:
+    import lidarr
+    HAS_LIDARR_LIBRARY = True
+except ImportError:
+    HAS_LIDARR_LIBRARY = False
+
+__metaclass__ = type
+
+
+def run_module():
+    result = dict(
+        changed=False,
+    )
+
+    module = LidarrModule(
+        argument_spec={},
+        supports_check_mode=True
+    )
+
+    client = lidarr.SystemApi(module.api)
+
+    # Get the system status.
+    try:
+        response = client.get_system_status()
+    except Exception as e:
+        module.fail_json('Error retrieving system status: %s' % to_native(e.reason), **result)
+
+    result.update(response)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
---
+++ plugins/modules/lidarr_tag.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2020, Fuochi <devopsarr@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: lidarr_tag
+
+short_description: Manages Lidarr tag.
+
+version_added: "1.0.0"
+
+description: Manages Lidarr tag.
+
+options:
+    label:
+        description: Actual tag.
+        required: true
+        type: str
+    state:
+        description: Create or delete a tag.
+        required: false
+        default: 'present'
+        choices: [ "present", "absent" ]
+        type: str
+
+extends_documentation_fragment:
+    - devopsarr.lidarr.lidarr_credentials
+
+author:
+    - Fuochi (@Fuochi)
+'''
+
+EXAMPLES = r'''
+# Create a tag
+- name: Create a tag
+  devopsarr.lidarr.tag:
+    label: default
+
+# Delete a tag
+- name: Delete a tag
+  devopsarr.lidarr.tag:
+    label: wrong
+    state: absent
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+id:
+    description: Tag ID.
+    type: int
+    returned: always
+    sample: '1'
+label:
+    description: The output message that the test module generates.
+    type: str
+    returned: 'on create/update'
+    sample: 'hd'
+'''
+
+from ansible_collections.devopsarr.lidarr.plugins.module_utils.lidarr_module import LidarrModule
+from ansible.module_utils.common.text.converters import to_native
+
+try:
+    import lidarr
+    HAS_LIDARR_LIBRARY = True
+except ImportError:
+    HAS_LIDARR_LIBRARY = False
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        # TODO: add validation for lowercase tags
+        label=dict(type='str', required=True),
+        state=dict(default='present', type='str', choices=['present', 'absent']),
+    )
+
+    result = dict(
+        changed=False,
+        id=0,
+    )
+
+    module = LidarrModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    client = lidarr.TagApi(module.api)
+
+    # List resources.
+    try:
+        tags = client.list_tag()
+    except Exception as e:
+        module.fail_json('Error listing tags: %s' % to_native(e.reason), **result)
+
+    # Check if a resource is present already.
+    for tag in tags:
+        if tag['label'] == module.params['label']:
+            result.update(tag)
+
+    # Create a new resource.
+    if module.params['state'] == 'present' and result['id'] == 0:
+        result['changed'] = True
+        # Only without check mode.
+        if not module.check_mode:
+            try:
+                response = client.create_tag(tag_resource={
+                    'label': module.params['label'],
+                })
+            except Exception as e:
+                module.fail_json('Error creating tag: %s' % to_native(e.reason), **result)
+            result.update(response)
+
+    # Delete the resource.
+    elif module.params['state'] == 'absent' and result['id'] != 0:
+        result['changed'] = True
+        # Only without check mode.
+        if not module.check_mode:
+            try:
+                response = client.delete_tag(result['id'])
+            except Exception as e:
+                module.fail_json('Error deleting tag: %s' % to_native(e.reason), **result)
+            result['id'] = 0
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
