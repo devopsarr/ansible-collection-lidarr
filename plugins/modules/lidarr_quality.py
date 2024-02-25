13fd612c8d5a01b89e6f78de6087924c5d62f1d8
Fuochi Filippo <filippo.fuochi@leitha.eu>
2024-02-25 19:51:23 +0100

    feat: add quality profiles mofule

---
+++ plugins/modules/lidarr_quality.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2020, Fuochi <devopsarr@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: lidarr_quality
+
+short_description: Manages Lidarr quality.
+
+version_added: "1.0.0"
+
+description: Manages Lidarr quality.
+
+options:
+    name:
+        description: Name.
+        required: true
+        type: str
+    title:
+        description: Title.
+        type: str
+    min_size:
+        description: Minimum size.
+        type: float
+    max_size:
+        description: Maximum size.
+        type: float
+
+
+extends_documentation_fragment:
+    - devopsarr.lidarr.lidarr_credentials
+
+author:
+    - Fuochi (@Fuochi)
+'''
+
+EXAMPLES = r'''
+---
+# update quality
+- name: Update quality
+  devopsarr.lidarr.lidarr_quality:
+    name: FLAC
+    title: FLAC
+    max_size: 200.0
+    min_size: 35.0
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+id:
+    description: Quality ID.
+    type: int
+    returned: always
+    sample: '1'
+title:
+    description: Title.
+    returned: always
+    type: str
+    sample: 'WEBRip-480p'
+min_size:
+    description: Minimum size.
+    returned: always
+    type: float
+    sample: '1.0'
+max_size:
+    description: Maximum size.
+    returned: always
+    type: float
+    sample: '130.0'
+quality:
+    description: Music folder format.
+    returned: always
+    type: dict
+'''
+
+from ansible_collections.devopsarr.lidarr.plugins.module_utils.lidarr_module import LidarrModule
+from ansible.module_utils.common.text.converters import to_native
+
+try:
+    import lidarr
+    HAS_LIDARR_LIBRARY = True
+except ImportError:
+    HAS_LIDARR_LIBRARY = False
+
+
+def init_module_args():
+    # define available arguments/parameters a user can pass to the module
+    return dict(
+        name=dict(type='str', required=True),
+        title=dict(type='str'),
+        min_size=dict(type='float'),
+        max_size=dict(type='float'),
+    )
+
+
+def list_qualities(result):
+    try:
+        return client.list_quality_definition()
+    except Exception as e:
+        module.fail_json('Error listing qualities: %s' % to_native(e.reason), **result)
+
+
+def find_quality(name, result):
+    for quality in list_qualities(result):
+        if quality.quality.name == name:
+            return quality
+    return None
+
+
+def update_quality(want, result):
+    result['changed'] = True
+    # Only without check mode.
+    if not module.check_mode:
+        try:
+            response = client.update_quality_definition(quality_definition_resource=want, id=str(want.id))
+        except Exception as e:
+            module.fail_json('Error updating quality: %s' % to_native(e.reason), **result)
+    # No need to exit module since it will exit by default either way
+    result.update(response.model_dump(by_alias=False))
+
+
+def run_module():
+    global client
+    global module
+
+    # Define available arguments/parameters a user can pass to the module
+    module = LidarrModule(
+        argument_spec=init_module_args(),
+        supports_check_mode=True,
+    )
+
+    # Init client and result.
+    client = lidarr.QualityDefinitionApi(module.api)
+    result = dict(
+        changed=False,
+        id=0,
+    )
+
+    # Check if a resource is present already.
+    state = find_quality(module.params['name'], result)
+    if state:
+        result.update(state.model_dump(by_alias=False))
+
+    # No delete is needed
+    want = lidarr.QualityDefinitionResource(
+        title=module.params['title'],
+        min_size=module.params['min_size'],
+        max_size=module.params['max_size'],
+    )
+
+    # Update an existing resource if needed.
+    want.id = result['id']
+    want.weight = result['weight']
+    want.quality = lidarr.Quality(**result['quality'])
+    if want != state:
+        update_quality(want, result)
+
+    # Exit whith no changes.
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
